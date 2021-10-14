89e79e5c06d009f52d5f2fc5231cd8bd92220f8f
KatyaTulit <ek.stupina@gmail.com>
2021-10-14 17:11:50 +0300

    tasks 1 (ChessPiece) and 2 (Horse)

---
+++ chess/src/ChessPiece.java
@@ -0,0 +1,19 @@
+abstract public class ChessPiece {
+    protected String color;
+    protected boolean check = true;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    abstract public String getColor();
+
+    abstract public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    abstract public String getSymbol();
+
+    public boolean checkPos(int pos) {
+        return pos >= 0 && pos <= 7;
+    }
+
+}
---
+++ chess/src/Horse.java
@@ -0,0 +1,28 @@
+import static java.lang.Math.abs;
+
+public class Horse extends ChessPiece{
+
+    public Horse(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+
+        if (checkPos(toLine) && checkPos(toColumn)) {
+            return ((abs(line-toLine) == 2 && abs(column-toColumn) == 1) || ((abs(line-toLine) == 1 && abs(column-toColumn) == 2)));
+        } else return false;
+
+    }
+
+    @Override
+    public String getSymbol() {
+        return "H";
+    }
+}
+
